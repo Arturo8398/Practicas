a4da1a0d452ab38a63abf2e63f260d75c859b102
Arturo8398 <agarcial043@alumnoauemex.mx>
2023-09-24 15:12:14 -0600

    Herencia

---
+++ Tareas/Herencia/Ejercicio2/Jugador.java
@@ -0,0 +1,28 @@
+package Tareas.Herencia.Ejercicio2;
+
+public class Jugador extends Personaje {
+    private String clase;
+
+    public Jugador(String nombre, int nivel, String clase) {
+        super(nombre, nivel);
+        this.clase = clase;
+    }
+
+    public Jugador() {
+    }
+
+    // get and set methods of this class
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    @Override
+    public String toString() {
+        return "- Nombre " + nombre + "\n- Nivel " + nivel + "\n- Clase " + clase;
+    }
+
+}
---
+++ Tareas/Herencia/Ejercicio2/Personaje.java
@@ -0,0 +1,55 @@
+package Tareas.Herencia.Ejercicio2;
+
+public class Personaje {
+    protected String nombre;
+    protected int nivel;
+
+    // Message
+    public void atacar() throws RuntimeException {
+        if (nivel < 5) {
+            throw new RuntimeException();
+        } else {
+            System.out.println(" \nIniciando ataque...\n");
+        }
+    }
+
+    public Personaje(String nombre, int nivel) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+    }
+
+    public Personaje() {
+    }
+
+    // own method of class Enemy
+    public void gritar() {
+        System.out.println("Ahhhhhhhh, vas a morir >:)");
+    }
+
+    // own method of class Player
+    public void usarHabilidadEspecial() {
+        System.out.println("Kame Hame Ha >:(");
+    }
+
+    // get and set methods of each attribute of the parent class
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Personaje [nombre=" + nombre + ", nivel=" + nivel + "]";
+    }
+}
